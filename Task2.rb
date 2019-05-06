4cdc37e9bf8f288d5716ab69693ab756c5f3ce4e
Jekyns <jekyns@mail.ru>
2019-05-06 15:19:53 +0300

    Update2

--- Task2.rb
+++ Task2.rb
@@ -8,7 +8,7 @@ a = Mechanize.new { |agent|
 page=a.get('https://alfabank.ru/ext-json/0.2/exchange/cash?offset=0&limit=2&mode=rest')
 
 #page.body.each_line {|str| puts str if str.include? 'class="h'}
-puts "-----------------------------------------------------------------"
+puts ""
 #puts page.body.each_char {|elem| print elem}
 var=page.body.to_json
 
@@ -16,4 +16,8 @@ var=var.split('\\')
 cost_usd=var[var.find_index('"value')+1]
 cost_usd=cost_usd.slice(2,cost_usd.length-3)
 puts "currency #{cost_usd}"
-puts ARGV[0].to_f*cost_usd.to_f
+unless ARGV[0].nil?
+	puts ARGV[0].to_f*cost_usd.to_f
+else
+	puts "Enter dollar sum"
+end
